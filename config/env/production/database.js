23066ac7beaddc7e421af52847e28b827e45aefd
Irina Zhigalova <Irina.Zhigalova@noveogroup.com>
2021-05-04 15:57:01 +0300

    Update production db

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,28 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
+// const parse = require('pg-connection-string').parse;
+// const config = parse(process.env.DATABASE_URL);
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: config.host,
+//         port: config.port,
+//         database: config.database,
+//         username: config.user,
+//         password: config.password,
+//         ssl: {
+//           rejectUnauthorized: false
+//         }
+//       },
+//       options: {
+//         ssl: true,
+//       },
+//     },
+//   },
+// });
 
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
@@ -7,18 +30,11 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false
-        }
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
-        ssl: true,
+        useNullAsDefault: true,
       },
     },
   },
