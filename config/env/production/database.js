aee08de320f7219457dd2aefb5e86a2729a7cfc7
Irina Zhigalova <Irina.Zhigalova@noveogroup.com>
2021-05-07 18:31:43 +0300

    Update production config

---
+++ config/env/production/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'iva-nova'),
+        username: env('DATABASE_USERNAME', 'iva-nova'),
+        password: env('DATABASE_PASSWORD', '1PDN80PfR_m-LjngcADCpagAwPDf_Gxk'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    },
+  },
+});
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,11 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('NGINX_URL', ''),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '08e37a0e581a580721aa22b587cb0aa0'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
-
-
