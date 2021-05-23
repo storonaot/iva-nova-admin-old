4898b8919fc3610856710b9d2aac5e79b9ea3743
Irina Zhigalova <Irina.Zhigalova@noveogroup.com>
2021-05-23 23:10:26 +0300

    Some model changes

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'storonaot'),
+        database: env('DATABASE_NAME', 'test'),
         username: env('DATABASE_USERNAME', 'storonaot'),
-        password: env('DATABASE_PASSWORD', ''),
+        password: env('DATABASE_PASSWORD', 'Durkheim135'),
       },
       options: {
         ssl: false,
