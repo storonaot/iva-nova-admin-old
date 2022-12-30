74a7e4de803cf39bcffcee32c950c15e4d7f4f9a
Irina Zhigalova <Irina.Zhigalova@noveogroup.com>
2022-12-30 16:02:43 +0300

    migrate API to v4 structure

---
+++ src/api/media-link/routes/media-link.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * media-link router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::media-link.media-link');
---
+++ v3/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9a6cd4ec-7d64-4e20-bf0d-8a463ceaa84c'
+};
