6c1936daf0692c3a861bbcf75c12bcab66f2bf0b
Irina Zhigalova <Irina.Zhigalova@noveogroup.com>
2022-12-30 15:59:22 +0300

    updated the config folder

--- config/server.js
+++ config/server.js
@@ -1,11 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '08e37a0e581a580721aa22b587cb0aa0'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
-
-
